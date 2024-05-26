5869422f6e400b9b0184e4b50f6af32db30a57a8
igorvins <igorvins@hotmail.com>
2024-05-26 19:39:45 -0300

    initial ideas

---
+++ places_for_dead_ghosts_KATA/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//For to iterate through each position
+
+    //if verify in wich strategic position it should be set
+
+        //if verify if the strategic position is empty or not
+            //empty? set the ghost there
+
+            //here the problem, if the previous one is not empty we shall look for 
+            //the closest one from the strategic position
+
+//return or consolo.print the array by default the empty spaces should be '_____'
+            
+            
