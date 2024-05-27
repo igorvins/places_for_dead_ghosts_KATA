16eda3778617f50df91d117565802f335851b2a1
igorvins <igorvins@hotmail.com>
2024-05-26 21:12:17 -0300

    initial logical schema

--- places_for_dead_ghosts_KATA/Program.cs
+++ places_for_dead_ghosts_KATA/Program.cs
@@ -1,17 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
 
+String[] input = { "Artlu", "Breca", "Cityl", "Dedaf" };
+char[] Earthenware = {'Q','U','T','H','C','R','D','M','Z'};
+char[] Waterfall = {'W','E','V','O','X','I','N','G'};
+char[] Fireplace = {'J','F','A','B','K','P','L','Y'};
+char[] Windowsill = {'S'};
 
 //For to iterate through each position
-
+for (int i=0; i<input.Length; i++)
+{
     //if verify in wich strategic position it should be set
+    char first_char = input[i][0];
+    if (Earthenware.Contains(first_char)) { }
+    else if (Waterfall.Contains(first_char)) { }
+    else if (Fireplace.Contains(first_char)) { }
+    else if (Windowsill.Contains(first_char)) { }
+    else { }
+}
 
-        //if verify if the strategic position is empty or not
-            //empty? set the ghost there
+//if verify if the strategic position is empty or not
+//empty? set the ghost there
 
-            //here the problem, if the previous one is not empty we shall look for 
-            //the closest one from the strategic position
+//here's the problem, if the previous one is not empty we shall look for 
+//the closest one from the strategic position
 
-//return or consolo.print the array by default the empty spaces should be '_____'
-            
-            
+//return or console.print the array, by default the empty spaces should be '_____'
+
