bfa6c4ce5469f371131c466cd5e2e6734cc17687
igorvins <igorvins@hotmail.com>
2024-05-26 21:23:56 -0300

    last updates

--- places_for_dead_ghosts_KATA/Program.cs
+++ places_for_dead_ghosts_KATA/Program.cs
@@ -5,21 +5,38 @@ char[] Earthenware = {'Q','U','T','H','C','R','D','M','Z'};
 char[] Waterfall = {'W','E','V','O','X','I','N','G'};
 char[] Fireplace = {'J','F','A','B','K','P','L','Y'};
 char[] Windowsill = {'S'};
+string[] output = new string[12];
 
 //For to iterate through each position
 for (int i=0; i<input.Length; i++)
 {
     //if verify in wich strategic position it should be set
     char first_char = input[i][0];
-    if (Earthenware.Contains(first_char)) { }
-    else if (Waterfall.Contains(first_char)) { }
-    else if (Fireplace.Contains(first_char)) { }
-    else if (Windowsill.Contains(first_char)) { }
+    string ghost_name = input[i];
+    if (Earthenware.Contains(first_char)) {
+        //verify if the 1 position is empty:
+        if (output[0] == "") { output[0] = ghost_name; }
+        else if () { }
+        else { }
+    }
+    else if (Waterfall.Contains(first_char)) {
+        if (output[3]=="") { output[0] = ghost_name; }
+        else if () { }
+        else { }
+    }
+    else if (Fireplace.Contains(first_char)) {
+        if (output[6]=="") { output[0] = ghost_name; }
+        else if () { }
+        else { }
+    }
+    else if (Windowsill.Contains(first_char)) {
+        if (output[9]=="") { output[0] = ghost_name; }
+        else if () { }
+        else { }
+    }
     else { }
 }
 
-//if verify if the strategic position is empty or not
-//empty? set the ghost there
 
 //here's the problem, if the previous one is not empty we shall look for 
 //the closest one from the strategic position
