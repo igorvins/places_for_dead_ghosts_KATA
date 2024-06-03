967e20133e6dc6fff07a07f56ad5a6e7ddd816fa
igorvins <igorvins@hotmail.com>
2024-06-03 01:52:00 -0300

    commitando ultimar alteraçoes

--- places_for_dead_ghosts_KATA/Program.cs
+++ places_for_dead_ghosts_KATA/Program.cs
@@ -1,60 +1,105 @@
 ﻿using System.Linq;
 
-String[] input = { "Artlu", "Breca", "Cityl", "Dedaf" };
+String[] input = { "Sevap", "Syolc", "Sgulg", "Stolb", "Sknoh", "Spord", "Sgnaf", "Shcat", "Sknit", "Snirg", "Senin", "Sliob" };
 char[] Earthenware = {'Q','U','T','H','C','R','D','M','Z'};
 char[] Waterfall = {'W','E','V','O','X','I','N','G'};
 char[] Fireplace = {'J','F','A','B','K','P','L','Y'};
 char[] Windowsill = {'S'};
 string[] output = new string[12];
-
+string[] output_2 = new string[12];
+int y = 0;
 //For to iterate through each position
 for (int i=0; i<input.Length; i++)
 {
     //if verify in wich strategic position it should be set
     char first_char = input[i][0];
     string ghost_name = input[i];
-    if (Earthenware.Contains(first_char)) {
+    if (i == 11 && ghost_name!= null) {
+        //check all indexes of output array and insert the ghost name at the remaining blanck one
+        for (int x = 0; x < input.Length; x++) {
+            if (output[x]==null) { output[x] = ghost_name; }
+        }
+    }
+    else if (Earthenware.Contains(first_char)) {
         //verify if the 1 position is empty:
         if (output[0] == null) { output[0] = ghost_name; }
-        else if (output[11] == null) { output[11] = ghost_name; }
-        else { }
+        else
+        {
+            int v = 0;
+            int j = 1;
+            while (v == 0)
+            {
+                if (output[12 - j] == null) { output[12 - j] = ghost_name; v = 1; }
+                else if (output[0 + j] == null) { output[0 + j] = ghost_name; v = 1; }
+                j++;
+            }
+        }
     }
     else if (Waterfall.Contains(first_char)) {
         if (output[3]== null) { output[3] = ghost_name; }
         else {
             //verify the closest position from 0 index where there's no ghosts:
             int v = 0;
+            int j = 1;
             while (v == 0)
             {
-                int j = 1;
-                if(output[3 - j]==null){ output[3 - j] = ghost_name;v = 1; }
+                //verifying if the next index exists:
+                if (9 - j < 0 || 9 + j > 12)
+                {
+                    if (output[12 - j] == null) { output[12 - j] = ghost_name; v = 1; }
+                    if (output[0 + j] == null) { output[0 + j] = ghost_name; v = 1; }
+                }
+                if (output[3 - j]==null){ output[3 - j] = ghost_name;v = 1; }
                 else if(output[3 + j]==null){ output[3 + j] = ghost_name;v = 1; }
                 j++;
             }
         }
     }
     else if (Fireplace.Contains(first_char)) {
-        if (output[6]==null) { output[6] = ghost_name; }
-        else if (output[5]== null) { output[5] = ghost_name; }
-        else { }
+        if (output[6] == null) { output[6] = ghost_name; }
+        else
+        {
+            int v = 0;
+            int j = 1;
+            while (v == 0)
+            {
+                if (output[6 - j] == null) { output[6 - j] = ghost_name; v = 1; }
+                else if (output[6 + j] == null) { output[6 + j] = ghost_name; v = 1; }
+                j++;
+            }
+        }
     }
     else if (Windowsill.Contains(first_char)) {
-        if (output[9]== null) { output[9] = ghost_name; }
-        else if (output[8]== null) { output[8] = ghost_name; }
-        else { }
+
+        if (output[9] == null) { output[9] = ghost_name;
+        }
+        else {
+            int v = 0;
+            int j = 1;
+            while (v == 0)
+            {
+                //verifying if the next index exists:
+                if ( 9 - j < 0 || 9 + j > 11 ) {
+                    if (output[12 - j] == null) { output[12 - j] = ghost_name; output_2[y] = ghost_name; v = 1; y++; }
+                    else if (output[0 + j] == null) { output[0 + j] = ghost_name; output_2[y] = ghost_name; v = 1; y++; }
+                }
+                else if (output[9 - j] == null) { output[9 - j] = ghost_name; output_2[y] = ghost_name;  v = 1; y++; }
+                else if (output[9 + j] == null) { output[9 + j] = ghost_name; output_2[y] = ghost_name; v = 1; y++; }
+                j++;
+            }
+        }
     }
-    else { }
 }
 //inserting _____ in blanck spaces
 for (int i = 0; i < output.Length; i++)
 {
     if (output[i] == null){output[i] = "_____";}
 }
+//printing the result
 for (int i = 0; i< output.Length; i++)
 {
-    Console.WriteLine(output[i]);       
+    Console.WriteLine(output_2[i]);       
 }
-
 //here's the problem, if the previous one is not empty we shall look for 
 //the closest one from the strategic position
 
